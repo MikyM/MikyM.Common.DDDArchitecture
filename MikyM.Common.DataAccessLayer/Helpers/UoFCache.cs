39898a65a08859c621ad4f956a507c948f024d8f
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2021-08-15 01:15:50 +0200

    fix: typecache refractor

--- MikyM.Common.DataAccessLayer/Helpers/UoFCache.cs
+++ MikyM.Common.DataAccessLayer/Helpers/UoFCache.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using MikyM.Common.DataAccessLayer.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using MikyM.Common.DataAccessLayer.Repositories;
 
 namespace MikyM.Common.DataAccessLayer.Helpers
 {
     public static class UoFCache
     {
-        public static List<Type> CachedTypes { get; } = AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(x => x.GetTypes()
-                .Where(t => t.BaseType == typeof(Repository<>) || t.BaseType == typeof(ReadOnlyRepository<>) ||
-                            t == typeof(ReadOnlyRepository<>)))
-            .ToList();
+        public static List<Type> CachedTypes { get; }
+
+        static UoFCache()
+        {
+            CachedTypes ??= AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes()
+                    .Where(t => t.BaseType == typeof(Repository<>) || t.BaseType == typeof(ReadOnlyRepository<>)))
+                .ToList();
+        }
     }
 }
