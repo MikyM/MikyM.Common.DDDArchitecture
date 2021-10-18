1acee47ff6386d45467ee0c887bbcdea7702c51a
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2021-10-18 10:37:52 +0200

    feat: DI extensions

---
+++ MikyM.Common.Application/DependancyInjectionExtensions.cs
@@ -0,0 +1,50 @@
+﻿// This file is part of MikyM.Common.DDDArchitecture project
+//
+// Copyright (C) 2021 Krzysztof Kupisz - MikyM
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using Autofac;
+using AutoMapper.Contrib.Autofac.DependencyInjection;
+using AutoMapper.Extensions.ExpressionMapping;
+using Microsoft.Extensions.DependencyInjection;
+using MikyM.Common.Application.Interfaces;
+using MikyM.Common.Application.Services;
+using System;
+
+namespace MikyM.Common.Application
+{
+    public static class DependancyInjectionExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IReadOnlyService<,>), typeof(ReadOnlyService<,>));
+            services.AddScoped(typeof(CrudService<,>), typeof(CrudService<,>));
+            services.AddAutoMapper(x =>
+            {
+                x.AddExpressionMapping();
+                x.AddMaps(AppDomain.CurrentDomain.GetAssemblies());
+            });
+        }
+
+        public static void AddApplicationLayer(this ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(ReadOnlyService<,>)).As(typeof(IReadOnlyService<,>))
+                .InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(CrudService<,>)).As(typeof(ICrudService<,>))
+                .InstancePerLifetimeScope();
+            builder.RegisterAutoMapper(opt => opt.AddExpressionMapping(), AppDomain.CurrentDomain.GetAssemblies());
+        }
+    }
+}
